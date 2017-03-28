f30a5e935189c4583f5d3f16b8c5f139a9722d44
Mittsy Tidwell <mtidwell506@gmail.com>
2017-03-28 16:31:07 -0500

    serious refactoring on the code was done in order to make the code reuseable, everything now toggles as it should

--- nested_list/nested_list.js
+++ nested_list/nested_list.js
@@ -3,138 +3,136 @@
 (function() {
 	var name = ["box1.txt", "box2.txt", "box3.txt", "box4.txt", "box5.txt", "box6.txt", "box7.txt", "box8.txt", "box9.txt", "box10.txt"];	
     
-    // // ------------------------------------------------------------------------
-    // // CREATES THE FIRST UL IN THE MAIN DIV
-    // // HOLDS THE LIST OF VMS AS BUTTONS AS LI
-    // // ------------------------------------------------------------------------
-    var main_node = document.createElement("UL");
-    main_node.setAttribute("id", "vm_name_list");
-    document.getElementById("main").appendChild(main_node);
-
-    // // ------------------------------------------------------------------------
-    // // ITERATES THROUGH THE NAME ARRAY CREATING THE LI AND BUTTONS FOR EACH
-    // // ------------------------------------------------------------------------
-    for (let i = 0; i < name.length; i++) {
-        // Creates the li inside the of the ul vm_name_list
-        var name_node = document.createElement("LI");
-        name_node.setAttribute("id", "vm_" + i);
-        main_node.appendChild(name_node); // appends the li to the ul
-
-        // creates a button inside of each li 
-        var btn_node = document.createElement("BUTTON");
-        btn_node.setAttribute("id", "btn_" + i);
-        btn_node.setAttribute("value", 0);
-
-        // displays vm name inside the list
-        var but_textnode = document.createTextNode(name[i]);
-        btn_node.appendChild(but_textnode); // appends the text to the button
-        name_node.appendChild(btn_node); // appends the button to the li
-
         // // ------------------------------------------------------------------------
-        // // SETS THE TOGGLE FOR EACH VM NAME
-        // // ------------------------------------------------------------------------
-        btn_node.addEventListener("click", function() {
+    // // CREATE THE UL LI BUTTONS AND TEXTNODES
+    // // create_element -> this will create the element and append it to the parent, sets the value to 0 until we read the file, then turning the value to 1 once the file is read
+    // // create_wo_append -> crates the element without appending it, sets the value to 0 until we read the file, then turning the value to 1 once the file is read
+    // // create_textnode -> places the text in the parent
+    // // ------------------------------------------------------------------------
+    function create_element(element, id_value, parent) {
+        element = document.createElement(element);
+        element.setAttribute("id", id_value);
+        element.setAttribute("value", 0);
+        document.getElementById(parent).appendChild(element);
+        return element;
+    }
+
+    function create_wo_append(element, id_value) {
+        element = document.createElement(element);
+        element.setAttribute("id", id_value);
+        element.setAttribute("value", 0);
+        return element;
+    }
+
+    function create_textnode(content, parent) {
+        var textnode = document.createTextNode(content);
+        document.getElementById(parent).appendChild(textnode);
+        return textnode;
+    }
+
+    // // ------------------------------------------------------------------------
+    // // CREATE THE VM NAME LIST WITH BUTTONS AND TEXTNODES
+    // // create the ul, li, and button for the vm's name list
+    // // ------------------------------------------------------------------------
+    function vm_name() {
+        create_element("ul", "vm_name_list", "main"); // (element, id_value, parent)
+
+        // creates the li, button, and textnode for the vm's name list
+        // then uses the event listener function to read the file and toggle the element
+        for (let i = 0; i < name.length; i++) {
+            create_element("li", "vm_" + i, "vm_name_list"); // (element, id_value, parent)
+            create_element("button", "btn_" + i, "vm_" + i); // (element, id_value, parent)
+            create_textnode(name[i], "btn_" + i); // (content, parent)
+            event_listener("btn_", i, name[i]); // (element, i)
+        }
+    }
+
+    // // ------------------------------------------------------------------------
+    // // EVENT LISTENER
+    // // event_listener -> used for the nested list of titles
+    // // nested_event_listener -> used for the detailed info on the vm's
+    // // ------------------------------------------------------------------------
+    function event_listener(element, i, name) {
+        var button = document.getElementById(element + i);
+        button.addEventListener("click", function() {
+            // checks the value of the element
             var value = parseInt(this.getAttribute("value"));
-            if (value == 0) {
-                // // ------------------------------------------------------------------------
-                // // CREATES THE UL FOR THE NESTED LIST
-                // // ------------------------------------------------------------------------
-                var info_list_node = document.createElement("UL");
-                info_list_node.setAttribute("id", "vm_info_list_" + i);
-                document.getElementById("vm_" + i).appendChild(info_list_node); // appends ul to li (name_node)
-                // // ------------------------------------------------------------------------
-                // // XMLHTTPREQUEST (READS THE FILE)
-                // // ------------------------------------------------------------------------
-                var xhr = new XMLHttpRequest(),
-                    method = "GET",
-                    url = "files/" + name[i];
-                xhr.open(method, url, true);
+            if (value === 0) {
+                create_element("ul", "nested_title_list_" + i, "vm_" + i);
 
-                xhr.onreadystatechange = function() {
-                    // checks the the page is fully loaded and status is ok
-                    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-                        var notes = xhr.responseText.split("\n");
-console.log(xhr)
-                        // // ------------------------------------------------------------------------
-                        // // READS THE FILE AND CREATES THE NESTED LIST 
-                        // // ------------------------------------------------------------------------
-                        for (let j = 0; j < notes.length; j++) {
-                            if (notes[j] == "Start**") {
-                                // j++ makes the line jump by 1, causing the button to display the title
-                                j++;
-                                // // ------------------------------------------------------------------------
-                                // // PLACES THE TITLE NAME AS BUTTONS
-                                // // ------------------------------------------------------------------------
-                                var info_node = document.createElement("LI");
-                                info_node.setAttribute("id", "vm_" + i + "_info_" + i);
-                                info_list_node.appendChild(info_node); // appends the li to the ul
-                                
-                                var btn_info_node = document.createElement("BUTTON");
-                                btn_info_node.setAttribute("id", "info_button_" + i);
-                                btn_info_node.setAttribute("value", 0);
-                                
-                                // j++ is used again here to jump by 1 line, causing the nested li to display the
-                                // detailed info only
-                                var info_textnode = document.createTextNode(notes[j++]);
-                                btn_info_node.appendChild(info_textnode); // appends the text to the button
-                                info_node.appendChild(btn_info_node); // appends the button to the li
+                read_file(name, i);
 
-                                // // ------------------------------------------------------------------------
-                                // // SETS THE TOGGLE FOR TITLE LIST (1ST NESTED LEVEL)
-                                // // ------------------------------------------------------------------------
-                                btn_info_node.addEventListener("click", function() {
-                                    var title_value = parseInt(this.getAttribute("value"));
-                                    if (title_value == 0) {
-                                        // // ------------------------------------------------------------------------
-                                        // // CREATES THE NESTED DETAILED INFO (2 NESTED LEVEL)
-                                        // // ------------------------------------------------------------------------
-                                        var detailed_info = document.createElement("UL");
-                                        detailed_info.setAttribute("id", "detailed_vm_info_" + j);
-                                        document.getElementById("vm_" + i + "_info_" + i).appendChild(detailed_info); // appends ul to li (info_node)
-
-                                        while (notes[j] != "End**") {
-                                            var detailed_info_node = document.createElement("LI");
-                                            detailed_info_node.setAttribute("id", "vm_detailed_info_" + j);
-                                            detailed_info.appendChild(detailed_info_node); // appends li to ul
-
-                                            var detailed_info_textnode = document.createTextNode(notes[j]);
-                                            detailed_info_node.appendChild(detailed_info_textnode); // appends text to li 
-console.log("j is in " + notes[j] + "\n" + j)
-                                            j++;
-                                        }
-                                        this.setAttribute("value", 1);
-console.log(this)
-                                    } else {
-                                        var temp = j-1;
-                                        var nested_toggle = document.getElementById("detailed_vm_info_" + temp);
-console.log("j in nested_toggle" + j)
-                                        if (nested_toggle.style.display != 'none') {
-                                            nested_toggle.style.display = 'none';
-                                        } else {
-                                            nested_toggle.style.display = '';
-                                        } // closes if inside else inside btn_info_node eventListner
-                                    } // closes the if inside the nested toggle
-                                }); // closes the nested toggle
-                            } // closes the if inside of for inside of if inside of onreadystatechange function
-                        } // closes the for loop inside if inside onreadystatechange function
-                    } // closes the if statement inside onreadystatechange function
-                }; // closes the onreadystatechange function
-                xhr.send();
-
-                // allows btn_node to be value of 1 so it will only load the file once and
-                // then toggle
                 this.setAttribute("value", 1);
-console.log(this)
             } else {
-                var toggle = document.getElementById("vm_info_list_" + i);
-                if (toggle.style.display != 'none') {
-                    // hides the vm_info_list
-                    toggle.style.display = 'none';
-                } else {
-                    // shows the vm_info_list
-                    toggle.style.display = '';
-                } // closes the if statement inside of the toggle
-            } // closes the if statement for the toggle
-        }); // closes toggle
-    } // closes the for loop for name
-})(); // closes the main function
+                toggle_element("nested_title_list_" + i);
+            }
+        });
+    }
+
+    function nested_event_listener(button_id, ul_id) {
+        var button = document.getElementById(button_id);
+        button.addEventListener("click", function() {
+            toggle_element(ul_id);
+        });
+    }
+
+    // // ------------------------------------------------------------------------
+    // // TOGGLE
+    // // ------------------------------------------------------------------------
+    function toggle_element(li) {
+        var toggle = document.getElementById(li);
+        if (toggle.style.display != "none") {
+            toggle.style.display = "none";
+        } else {
+            toggle.style.display = "";
+        }
+    }
+
+    // // ------------------------------------------------------------------------
+    // // READ FILE
+    // // reads the file once the page is loaded and status is ok
+    // // ------------------------------------------------------------------------
+    function read_file(name, i) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                // splits the file on each new line
+                var notes = this.responseText.split("\n");
+
+                for (let j = 0; j < notes.length; j++) {
+                    if (notes[j] == "Start**") {
+                        j++;
+                        // CREATES LI, BUTTON, AND TEXTNODE FOR NESTED TITLE
+                        // j and i are used here in order to keep the right file under the right vm 
+                        create_element("li", "nested_title_" + j + "_vm_" + i, "nested_title_list_" + i); // (element, id_value, parent)
+                        create_element("button", "nested_title_btn_" + j + "_vm_" + i, "nested_title_" + j + "_vm_" + i); // (element, id_value, parent)
+                        create_textnode(notes[j], "nested_title_btn_" + j + "_vm_" + i); // (content, parent)  
+                            
+                        // CREATES THE UL AND LI FOR THE DETAILED NOTES
+                        // j and i are used here in order to keep the right file under the right vm 
+                        // set detailed_ul style to none so it will not show it until clicked
+                        create_element("ul", "detailed_notes_" + j + "_vm_" + i, "nested_title_" + j + "_vm_" + i); // (element, id_value, parent)
+                        var detailed_ul = document.getElementById("detailed_notes_" + j + "_vm_" + i);   
+                        detailed_ul.setAttribute("style", "display: none;");
+                        nested_event_listener("nested_title_btn_" + j + "_vm_" + i, "detailed_notes_" + j + "_vm_" + i);
+                        j++;
+
+                        var result = "";
+
+                        // &lt and <pre> is used to keep the true format of the file that is read
+                        while (notes[j] != "End**") {
+                            var detailed_notes = notes[j].replace("<", "&lt");
+                            result += "<li><pre>" + detailed_notes + "</pre></li>";
+                            j++;
+                        }
+                        detailed_ul.innerHTML = result;
+                    }
+                }
+            }
+        };
+        xhttp.open("GET", "files/" + name, true);
+        xhttp.send();
+    }
+
+vm_name();
+})();
